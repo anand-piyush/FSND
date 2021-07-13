fa958017ae879252cfbae76c42a87a25f2868a1b
Piyush Anand <piyush@Piyushs-MacBook-Pro.local>
2021-07-13 07:11:37 +0530

    endpoints added and front end brought to work

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -18,6 +18,7 @@ def create_app(test_config=None):
   setup_db(app)
   
   
+  # TODO : COMMENT OR DELETE THE BELOW METHOD WHEN PROJECT COMPLETES
   @app.route('/api/v1.0/')
   @app.route('/')
   def main():
@@ -40,57 +41,159 @@ def create_app(test_config=None):
     response.headers.add('Access-Control-Allow-Headers', 'GET, POST, PATCH, DELETE, OPTION')
     return response
 
+  def query_categories():
+    '''
+    method to query categories from db
+    '''
+    categories = Category.query.all()
+    cat_dict = {cat.id:cat.type for cat in categories}
+    return cat_dict
 
   '''
-  @TODO: 
-  Create an endpoint to handle GET requests 
-  for all available categories.
+  @TODo Done
   '''
+  @app.route('/api/v1.0/categories')
+  def get_categories():
+    ''' 
+    an endpoint to handle GET requests 
+    for all available categories.
+    '''
+    try:
+      cat_dict = query_categories()
+      return jsonify({
+          "success": True,
+          "categories": cat_dict
+      })
+    except Exception as e:
+      print(e)
+      abort(500)  
+    
 
-
-  '''
-  @TODO: 
-  Create an endpoint to handle GET requests for questions, 
-  including pagination (every 10 questions). 
-  This endpoint should return a list of questions, 
-  number of total questions, current category, categories. 
-
-  TEST: At this point, when you start the application
-  you should see questions and categories generated,
-  ten questions per page and pagination at the bottom of the screen for three pages.
-  Clicking on the page numbers should update the questions. 
-  '''
-
-  '''
-  @TODO: 
-  Create an endpoint to DELETE question using a question ID. 
-
-  TEST: When you click the trash icon next to a question, the question will be removed.
-  This removal will persist in the database and when you refresh the page. 
-  '''
+  
 
   '''
-  @TODO: 
-  Create an endpoint to POST a new question, 
-  which will require the question and answer text, 
-  category, and difficulty score.
+  @TODo Done: 
+  '''
+  @app.route('/api/v1.0/questions', methods=['GET'])
+  def get_questions():
+    '''
+    an endpoint to handle GET requests for questions, 
+    including pagination (every 10 questions). 
+    This endpoint should return a list of questions, 
+    number of total questions, current category, categories. 
 
-  TEST: When you submit a question on the "Add" tab, 
-  the form will clear and the question will appear at the end of the last page
-  of the questions list in the "List" tab.  
+    TEST: At this point, when you start the application
+    you should see questions and categories generated,
+    ten questions per page and pagination at the bottom of the screen for three pages.
+    Clicking on the page numbers should update the questions. 
+
+    # test case 1 : check if the returned questions > 0
+    # test case 2 : pagination
+    '''
+    questions = Question.query.all()
+    total_questions = len(questions)
+
+    ques_list = paginate(request, questions)
+    cat_dict = query_categories()
+    
+    if len(ques_list) == 0:
+        # page requested is beyond the available pages
+        abort(404)
+
+    return jsonify({
+        'success': True,
+        'questions': ques_list,
+        'total_questions': total_questions,
+        'categories': cat_dict,
+        'current_category': None
+    })
+
+  '''
+  @TODo Done: 
+  '''
+  @app.route("/api/v1.0/questions/<int:question_id>", methods=['DELETE'])
+  def delete_question(question_id):
+    """
+    an endpoint to DELETE question using a question ID. 
+    TEST: When you click the trash icon next to a question, the question will be removed.
+    This removal will persist in the database and when you refresh the page. 
+    """
+    question = Question.query.get(question_id)
+    success = False
+    if not question:
+      abort(404)
+    else:
+      try:
+        question.delete()
+        success = True
+      except:
+        # unable to process
+        abort(422)
+      return jsonify({
+                    'success': success,
+                    'deleted': question_id
+                })
+
+
+
+
+  '''
+  @TODo Done:
   '''
 
-  '''
-  @TODO: 
-  Create a POST endpoint to get questions based on a search term. 
-  It should return any questions for whom the search term 
-  is a substring of the question. 
+  @app.route('/api/v1.0/questions', methods=['POST'])
+  def add_question():
+    '''
+    an endpoint to POST a new question, 
+    which will require the question and answer text, 
+    category, and difficulty score.
 
-  TEST: Search by any phrase. The questions list will update to include 
-  only question that include that string within their question. 
-  Try using the word "title" to start. 
-  '''
+    if searchTerm is given, a POST endpoint to get questions based on a search term. 
+    It should return any questions for whom the search term 
+    is a substring of the question. 
 
+    TEST: Search by any phrase. The questions list will update to include 
+    only question that include that string within their question. 
+    Try using the word "title" to start.
+
+    TEST: When you submit a question on the "Add" tab, 
+    the form will clear and the question will appear at the end of the last page
+    of the questions list in the "List" tab.  
+    '''
+    form_data = request.json
+    if "searchTerm" in form_data:
+        search_term = form_data['searchTerm'].strip()
+        questions = Question.query.filter(Question.question.ilike(f'%{search_term}%')).all() 
+        
+        # paginate not supported at frontend, so all results returned
+        q_list = [q.format() for q in questions]
+
+        return jsonify({
+            "success": True,
+            "questions": q_list
+        })
+    
+    else:
+        if (form_data['question'].strip() == "") or (form_data['answer'].strip() == ""):
+            # Empty Values
+            abort(400)
+
+        try:
+            ques = form_data['question'].strip()
+            ans = form_data['answer'].strip()
+            cat = form_data['category']
+            difficulty = form_data['difficulty']
+            new_question = Question(question=ques, answer=ans,category=cat, difficulty=difficulty)
+            new_question.insert()
+        except:
+            abort(422)
+
+        return jsonify({
+            "success": True,
+            "added": new_question.id
+        })
+
+  
   '''
   @TODO: 
   Create a GET endpoint to get questions based on category. 
@@ -114,10 +217,41 @@ def create_app(test_config=None):
   '''
 
   '''
-  @TODO: 
-  Create error handlers for all expected errors 
+  @TODo Done: 
+  Error handlers for all expected errors 
   including 404 and 422. 
   '''
+  @app.errorhandler(400)
+  def bad_request(error):
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "Bad Request"
+    }),400
+
+  @app.errorhandler(404)
+  def not_found(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "Not found"
+    }),404
+
+  @app.errorhandler(422)
+  def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 422,
+        "message": "Unprocessable Entity"
+    }),422
+
+  @app.errorhandler(500)
+  def internal_server_error(error):
+    return jsonify({
+        "success": False,
+        "error": 500,
+        "message": "Internal Server Error"
+    }),500
   
   return app
 
